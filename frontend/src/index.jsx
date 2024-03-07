45e07982dcbb925b41621e7f53fe0f7d49c84e3a
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-03-07 14:57:00 +0000

    done

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,9 +4,6 @@ import EmployeeForm from './Components/AddEmployeeForm';
 import DepartmentEmployees from './Components/DepartmentEmployees';
 
 export function App() {
- 
-  
-
   return (
     <div>
       <h1 style={{textAlign:'center'}}>Payroll Management System</h1>
